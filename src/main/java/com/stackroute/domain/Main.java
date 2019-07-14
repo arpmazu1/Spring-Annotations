7cd925d2c48386a0a055af11185d8545f3da439e
Arpita <arpita.mazumder02@gmail.com>
2019-07-14 23:16:27 +0530

    Committing Spring with Annotations

--- src/main/java/com/stackroute/domain/Main.java
+++ src/main/java/com/stackroute/domain/Main.java
@@ -2,15 +2,20 @@ package com.stackroute.domain;
 
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
         //for accessing Config.class
-        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-        // Using Autowire byName
-        Movie movie1 = context.getBean("movie1", Movie.class);
-        movie1.display();
+        ConfigurableApplicationContext configurableApplicationContext = new AnnotationConfigApplicationContext(Config.class);
+
+        //for BeanLifecycleDemoBean
+        BeanLifecycleDemoBean beanLifecycleDemoBean = configurableApplicationContext.getBean(BeanLifecycleDemoBean.class);
+
+
+        //closing down the application
+        configurableApplicationContext.registerShutdownHook();
     }
 }
